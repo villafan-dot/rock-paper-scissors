360f8bb7d18607be311ccc2a7682be7879cfa349
villafan-dot <chriswhy95@gmail.com>
2025-08-13 16:22:56 -0700

    made the game playable with keys

--- scripts/rock-paper-scissors.js
+++ scripts/rock-paper-scissors.js
@@ -43,6 +43,20 @@
       return computerMove;
     }
 
+    document.body.addEventListener('keydown', (event) => {
+      if(event.key === 'r'){
+    
+        playGame('Rock')
+      }
+      else if(event.key === 'p'){
+        playGame('Paper')
+      }
+      else if(event.key === 's'){
+        playGame('Scissors')
+      };
+    });
+    
+
     function playGame(playerMove) {
 
       const computerMove = pickComputerMove();
@@ -139,5 +153,21 @@
       }
       
     }
+
+
+    document.querySelector('.js-rock-button')
+    .addEventListener('click',() => {
+      playGame('Rock');
+    });
+    document.querySelector('.js-paper-button')
+    .addEventListener('click', () => {
+      playGame('Paper');
+    });
+    
+    document.querySelector('.js-scissor-button')
+    .addEventListener('click', () => {
+      playGame('Scissors');
+    });
+    
     
         
